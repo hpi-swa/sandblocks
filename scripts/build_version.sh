65f692cb6d1394d947628509103e33bde0e8317a
Tom Beckmann <tomjonabc@gmail.com>
2023-04-14 14:15:44 +0200

    update builder script

--- scripts/build_version.sh
+++ scripts/build_version.sh
@@ -17,17 +17,19 @@ COG_VM_PARAM="-nosound -vm-display-null"
 mkdir -p "${DEPLOY_PATH}" 
 cd "${DEPLOY_PATH}"
 
-# BASE="Squeak5.3-19458-64bit-202003021730-Linux"
-BASE="Squeak5.3-19458-64bit-All-in-One"
+
+
+
+BASE="Squeak6.0-22104-64bit-All-in-One"
 BASE_SHARED="$BASE.app"
 
 print_info "Downloading $BASE image..."
-wget --no-verbose "http://files.squeak.org/5.3/Squeak5.3-19458-64bit/$BASE.zip"
+wget --no-verbose "http://files.squeak.org/6.0/Squeak6.0-22104-64bit/$BASE.zip"
 unzip -q $BASE.zip
 rm "$BASE.zip"
 
 print_info "Preparing $BASE image..."
-$BASE_SHARED/Contents/Linux-x86_64/bin/squeak $COG_VM_PARAM "$BASE_SHARED/Contents/Resources/Squeak5.3-19458-64bit.image" "$(pwd)/../scripts/prepare_image.st" || EXIT_STATUS=$?
+$BASE_SHARED/Contents/Linux-x86_64/bin/squeak $COG_VM_PARAM "$BASE_SHARED/Contents/Resources/Squeak6.0-22104-64bit.image" "$(pwd)/../scripts/prepare_image.st" || EXIT_STATUS=$?
 
 print_info "Zipping package ..."
 if [[ $EXIT_STATUS -eq 0 ]]; then
